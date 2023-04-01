7c07185062506a61301606eb5c8599cdf1852b59
jedidiah1207 <sec20cs061@sairamtap.edu.in>
2023-04-01 23:00:04 +0530

    Changed the name of the function in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const remCount=document.getElementById("rcount");
 txtArea.addEventListener("keyup",()=>{
     update();
 })
-function update(){
+function updateCounter(){
    charCount.innerText=txtArea.value.length;
     remCount.innerText=txtArea.getAttribute("maxLength")- txtArea.value.length;
 
